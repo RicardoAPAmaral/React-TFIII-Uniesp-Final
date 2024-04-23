4931e17f7029e9f3e9b7f917a89cd0eb4b4e46b2
Diego Patrĩcio <diegopatricio.qe@gmail.com>
2024-04-22 23:32:44 -0300

    Adiciona Spring Security

--- src/main/java/br/com/diegopatricio/servicex/Config/SecurityConfig.java
+++ src/main/java/br/com/diegopatricio/servicex/Config/SecurityConfig.java
@@ -1,18 +1,29 @@
-/*package br.com.diegopatricio.servicex.Config;
+package br.com.diegopatricio.servicex.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -25,26 +36,4 @@ public class SecurityConfig {
                 .password(passwordEncoder.encode("987654"))
                 .roles("USER", "ADMIN");;
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Configuration
-    public static class WebSecurityConfig {
-
-        @Autowired
-        public void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests()
-                    .requestMatchers(HttpMethod.GET, "/categorias/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/categorias").hasRole("USER")
-                    .anyRequest()
-                    .authenticated()
-                    .and()
-                    .csrf().disable()
-                    .httpBasic();
-        }
-    }
-}*/
+}
